00a21f82aa63c4163afaad1136a16eccd630da35
Sakshi <isakshy18@gmail.com>
2022-02-11 16:28:04 +0530

    text-editing control added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -23,6 +25,7 @@ class MyCustomForm extends StatefulWidget {
   @override
   MyCustomFormState createState() {
     return MyCustomFormState();
+    State<StatefulWidget> createState() => new MyCustomFormState();
   }
 }
 
@@ -33,12 +36,17 @@ class MyCustomFormState extends State<MyCustomForm> {
   final _formKey = GlobalKey<FormState>();
   final items = ['category1', 'category2', 'category3'];
   String? value;
+  TextEditingController amountController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+
   @override
   DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
         value: item,
         child: Text(
           item,
-          style: TextStyle(fontWeight: FontWeight.bold),
+          style: TextStyle(
+              //fontWeight: FontWeight.bold,
+              color: Colors.black87),
         ),
       );
 
@@ -49,47 +57,98 @@ class MyCustomFormState extends State<MyCustomForm> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: const Icon(Icons.calendar_view_day_rounded),
-              hintText: 'Enter Date',
-              labelText: 'Date',
+          //Date
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 30, 50, 0),
+            child: TextFormField(
+              controller: dateController,
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.calendar_view_day_rounded),
+                hintText: 'Enter Date',
+                labelText: 'Date',
+              ),
+              validator: (String? value) {
+                if (value == null || value!.isEmpty) {
+                  return 'Please enter valid date';
+                }
+                return null;
+              },
             ),
-            validator: (String? value) {
-              if (value!.isEmpty) {
-                return 'Please enter valid date';
-              }
-              return null;
-            },
           ),
-          TextFormField(
-            decoration: const InputDecoration(
-              fillColor: Colors.brown,
-              focusColor: Colors.black,
-              icon: const Icon(Icons.money),
-              hintText: 'Enter amount',
-              labelText: 'Amount',
+
+          //Kharcha
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 10, 50, 0),
+            child: TextFormField(
+              controller: amountController,
+              decoration: const InputDecoration(
+                fillColor: Colors.brown,
+                focusColor: Colors.black,
+                icon: const Icon(Icons.money),
+                hintText: 'Enter amount',
+                labelText: 'Amount',
+              ),
+              validator: (String? value) {
+                if (value!.isEmpty) {
+                  return 'Please enter valid amount';
+                }
+                return null;
+              },
             ),
-            validator: (String? value) {
-              if (value!.isEmpty) {
-                return 'Please enter valid amount';
-              }
-              return null;
-            },
           ),
-          Center(
-            child: DropdownButton<String>(
-              items: items.map(buildMenuItem).toList(),
-              onChanged: (value) => setState(
-                () => this.value = value,
+
+          //category
+          Container(
+            margin: EdgeInsets.fromLTRB(50, 20, 50, 0),
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blueAccent,
+                width: 2,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Center(
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  hint: Text("Select Category"),
+                  isExpanded: true,
+                  value: value,
+                  items: items.map(buildMenuItem).toList(),
+                  onChanged: (value) => setState(
+                    () => this.value = value,
+                  ),
+                ),
               ),
             ),
           ),
-          new Container(
-              padding: const EdgeInsets.only(left: 150.0, top: 40.0),
-              child: new RaisedButton(
-                child: const Text('Submit'),
+
+          //Save
+          Container(
+            //height: 50,
+            // decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.blueAccent, width: 2,),
+            //     borderRadius: BorderRadius.circular(50)
+            // ),
+            margin: EdgeInsets.fromLTRB(120, 50, 120, 0),
+            //padding: EdgeInsets.fromLTRB(50, 0, 10, 0),
+            //padding: EdgeInsets.only(left: 220, top: 20),
+            child: Center(
+              child: TextButton(
+                child: Text("Save", style: TextStyle(fontSize: 15)),
+                style: ButtonStyle(
+                  // margin: EdgeInsets.all(100),
+                  padding:
+                      MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          side: BorderSide(color: Colors.red))),
+                ),
                 onPressed: () {
+                  print(dateController.text);
+                  print(amountController.text);
                   // It returns true if the form is valid, otherwise returns false
                   if (_formKey.currentState!.validate()) {
                     // If the form is valid, display a Snackbar.
@@ -97,7 +156,28 @@ class MyCustomFormState extends State<MyCustomForm> {
                         SnackBar(content: Text('Data is in processing.')));
                   }
                 },
-              )),
+              ),
+            ),
+
+            // RaisedButton(
+            //
+            //   child: Center(child: const Text('Save',
+            //   style: TextStyle(letterSpacing: 1,
+            //       fontWeight: FontWeight.bold),
+            //       ),
+            //   ),
+            //
+            //     onPressed: () {
+            //       // It returns true if the form is valid, otherwise returns false
+            //       if (_formKey.currentState!.validate()) {
+            //         // If the form is valid, display a Snackbar.
+            //         Scaffold.of(context).showSnackBar(
+            //             SnackBar(content: Text('Data is in processing.')));
+            //       }
+            //     },
+            //
+            //   ),
+          ),
         ],
       ),
     );
